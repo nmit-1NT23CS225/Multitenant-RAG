5466d716c062d5ecff22861a71f18397f05a1fbd
Hari <1nt23cs225.shreehari@nmit.ac.in>
2026-04-14 10:20:26 +0530

    Added core.db connection file and scripts.test_db

---
+++ core/db.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+def get_connection():
+    conn=psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+    
+    return conn
+    
---
+++ scripts/test_db.py
@@ -0,0 +1,20 @@
+from core.db import get_connection
+
+def test_connection():
+    try:
+        conn=get_connection()
+        cursor=conn.cursor()
+        cursor.execute("SELECT VERSION();")
+        version=cursor.fetchone()
+        print("Connection successful")
+        print(f"Postgres version is {version[0]}")
+        cursor.close()
+        conn.close()
+        print("Connection closed")
+
+    except Exception as e:
+        print(f"Connection failed: {e}")
+
+if __name__ == "__main__":
+    
+    test_connection()
